eb9a2199859d06c5af0078e40e3912c7d293bf86
Ukeh Victor <victorukeh1@gmail.com>
2022-07-15 13:35:01 +0100

    trying

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=victor
+export POSTGRES_PASSWORD=superfly
+export POSTGRES_HOST=project-3-dev.c4kotlgv5jcg.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=project-3-dev
+export AWS_REGION=us-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=tsadasdasdsfdsfss
+export URL=http://localhost:8100

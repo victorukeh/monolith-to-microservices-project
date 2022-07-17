f7a11cdfbb2c25c841d70dbd6ad909e265e6b963
Ukeh Victor <victorukeh1@gmail.com>
2022-07-17 14:43:07 +0100

    changed localhost to exposed ip

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a517879a1a18c408180c240bba2e1abe-1488613009.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8da96b3c9dd64229bd114339304ec45-2054474221.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
